6453e128017a3394d53224fadfed38174be360a3
Emmanuel <olatunjie335@gmail.com>
2025-09-21 20:15:40 +0100

    Add Assignment7.py file

---
+++ OneDrive/Documents/Python assignments/Python chapter 2 assignment/Assignment7.py
@@ -0,0 +1,13 @@
+number1 = 1024
+number2 = 2
+if number1 % 4 == 0:
+	print('Multiple of 4')
+if number1 % 4 != 0:
+	print('Not a multiple of 4')
+
+if number2 % 10 == 0:
+	print( 'Multiple of 10')
+if number2 % 10 != 0:
+	print( 'Not a multiple of 10')
+
+
