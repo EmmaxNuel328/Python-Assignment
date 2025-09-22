d1f592f8223a4e3d28bf6eb4290d5c2db6e83439
Emmanuel <olatunjie335@gmail.com>
2025-09-22 08:22:48 +0100

    Add Assignment11.py file

---
+++ OneDrive/Documents/Python assignments/Python chapter 2 assignment/Assignment11.py
@@ -0,0 +1,8 @@
+number = int(input('Enter five digit number'))
+number1 = number//10000
+number2 = (number%10000)//1000
+number3 = ((number%1000)%1000)//100
+number4 = (((number%10000)%1000)%100)//10
+number5 = number%10
+
+print(number1,number2,number3,number4,number5)
