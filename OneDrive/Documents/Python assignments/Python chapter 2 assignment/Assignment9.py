b559e2dc059fead99957620f4947ede505598716
Emmanuel <olatunjie335@gmail.com>
2025-09-22 08:20:06 +0100

    Add Assignment9.py file

---
+++ OneDrive/Documents/Python assignments/Python chapter 2 assignment/Assignment9.py
@@ -0,0 +1,30 @@
+
+In[1]: ord('B')
+Out[1]: 66
+ In[2]: ord('C')
+Out[2]: 67
+In[3]:  ord('D')
+Out[3]: 68
+In[4]: ord('b')
+Out[4]: 98
+In[5]: ord('c')
+Out[5]: 99
+In[6]: ord('d')
+Out[6]: 100
+In[7]: ord('0')
+Out[7]: 48
+In[8]: ord('1')
+Out[8]: 49
+In[9]: ord('2')
+Out[9]: 50
+In[10]: ord('$')
+Out[10]: 36
+In[11]: ord('*')
+Out[11]: 42
+In[12]:  ord('+')
+Out[12]: 43
+In[13]: ord(' ')
+Out[12]: 32
+
+
+
