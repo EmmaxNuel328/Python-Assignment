41bc23a7f0246174eecd7499be32f01ca98f860a
Emmanuel <olatunjie335@gmail.com>
2025-09-22 08:23:44 +0100

    Assignment12.py file

---
+++ OneDrive/Documents/Python assignments/Python chapter 2 assignment/Assignment12.py
@@ -0,0 +1,12 @@
+principal = 1000
+rate = 7/100
+years1 = 10
+years2 = 20
+years3 = 30
+
+amount1 = int(principal*(1 + rate)**years1)
+print('Amount after 10 years', '=', amount1)
+amount2 = int(principal*(1 + rate)**years2)
+print('Amount after 20 years', '=', amount2)
+amount3 = int(principal*(1 + rate)**years3)
+print('Amount after 3o years', '=', amount3)
