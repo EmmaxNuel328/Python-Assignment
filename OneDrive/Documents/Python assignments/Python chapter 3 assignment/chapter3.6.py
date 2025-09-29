1828059bc0c128257d2615f04e0526591eb34f5a
Emmanuel <olatunjie335@gmail.com>
2025-09-29 09:42:05 +0100

    Add chapter3.6.py file

---
+++ OneDrive/Documents/Python assignments/Python chapter 3 assignment/chapter3.6.py
@@ -0,0 +1,7 @@
+problem = input('what is your problem?')
+second = input('Have you had this problem before(Yes or No)')
+
+if second =='Yes':
+	print('Well, you have it again')
+else: 
+	print('Well, you have it now')
