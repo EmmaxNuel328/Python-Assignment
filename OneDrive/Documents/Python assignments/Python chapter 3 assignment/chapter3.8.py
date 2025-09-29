c40f86ae4ad5c2158c10eb49293b264e8a42c9d7
Emmanuel <olatunjie335@gmail.com>
2025-09-29 10:54:32 +0100

    Add chapter3.8.py file

---
+++ OneDrive/Documents/Python assignments/Python chapter 3 assignment/chapter3.8.py
@@ -0,0 +1,23 @@
+
+sum = 0
+average = sum/4
+product = 1
+for integer in range(4):
+	integer = int(input('Enter an integer:'))
+	sum = sum + integer
+	product = product * integer
+	average = sum/4
+largest = integer
+if integer > integer:
+	largest = integer
+if integer > integer:
+	largest = integer
+if integer > integer:
+	largest = integer
+if integer > integer:
+	largest = integer
+	
+print('Sum =', sum)
+print('Average =', average)
+print('Product =', product)
+print(largest)
